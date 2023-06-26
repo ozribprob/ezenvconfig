8801fb2e1694e9eb909a4e37dff40d47cdab46c1
ozribprob <oribeiro@problemcompany.org>
2023-06-26 18:51:49 +0000

    Documenting the Entry type

--- config.go
+++ config.go
@@ -5,12 +5,22 @@ import (
 	"os"
 )
 
+// Entry describes an env var entry
 type Entry struct {
-	Name       string
-	Aliases    []string
+	// Name is the name of the entry
+	Name string
+	// Aliases describes the aliases for the entry
+	Aliases []string
+	// OnNotFound is executed when the entry is not found.
+	// It's executed when the package couldn't find the entry.
+	// It's executed with the default flag too.
+	// It doesn't stops the error returning in  ExtractFromEnv when it's not optional.
 	OnNotFound func()
-	Default    string
-	Optional   bool
+	// Default describes the default value.
+	Default string
+	// Optional describes if the entry is optional.
+	// If true, the ExtractFromEnv doesn't return an error when the entry is not found.
+	Optional bool
 }
 
 var (
